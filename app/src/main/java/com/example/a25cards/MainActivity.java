15172da6aefa59bb61897a988da6b05bb607e78f
201721123044-周炳辉 <1621360512@qq.com>
2019-11-01 21:42:23 +0800

    first change

--- app/src/main/java/com/example/a25cards/MainActivity.java
+++ app/src/main/java/com/example/a25cards/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.a25cards;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.pm.ActivityInfo;
+import android.view.Window;
+import android.view.WindowManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 全屏、隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // 横屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 }
